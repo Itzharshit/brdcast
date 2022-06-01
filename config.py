4fa653d1765daebc18973a909051045c34a394db
Harshit Shrivastav <87630299+Itzharshit@users.noreply.github.com>
2022-06-01 09:10:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-DB_URL = os.environ.get("DB_URL", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5127131547:AAFlH9vA_oNcqwQi985TzBRSyAL9svrYCrs")
+API_ID = int(os.environ.get("API_ID", "17378766"))
+API_HASH = os.environ.get("API_HASH", "64363ed3e29119c36ff4ef28deac8645")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001600759370"))
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1607263889").split())
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://harshit:harshit@cluster0.ry72g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
